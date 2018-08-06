728695891eb39cda696fced1e4079ff649b5ec86
AFresnedo <AFresnedo@users.noreply.github.com>
2018-08-06 09:35:00 -0700

    update README to design notes

---
+++ js/ammo.js
@@ -0,0 +1,12 @@
+/* This script contains all the different ammo used by the game. Information
+ * about cost, power range, weight, and other weapon-specific values are stored
+ * in the objects for each weapon type.
+ */
+
+class Tracer {}
+
+class Standard {}
+
+class Chain {}
+
+class Nuke {}
---
+++ js/players.js
@@ -0,0 +1,17 @@
+/*
+ * This script contains the objects and functions related to players. Most
+ * importantly, the player object. The player object has all the data that is
+ * shared in type, but specific in value, to each player.
+ *
+ * Created by Andres Fresnedo
+ */
+
+class Player {
+  constructor(name, color, inventory, score) {
+    this.name = name;
+    this.color = color;
+    // TODO ensure object
+    this.inventory = inventory;
+    this.score = score;
+  }
+}
