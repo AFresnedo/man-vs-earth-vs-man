2a4bf61fa3cfde95a5236e132cd7d8319ec827ed
AFresnedo <AFresnedo@users.noreply.github.com>
2018-08-08 17:16:37 -0700

    fix array of unit(s) bug

--- js/units.js
+++ js/units.js
@@ -1,14 +1,15 @@
 // on a timer, drops all tanks from ceiling
 function spawnUnits() {
   console.log('dropping tanks');
-  for (var i = 0; i < 3; i++) {
+  console.log('playerCount, in units, is', playerCount.value);
+  for (var i = 0; i < playerCount.value; i++) {
     console.log('dropping tank', i);
     // create new unit
-    var unit = game.add.sprite(32, GAME_HEIGHT - 32, 'tank');
-    // assign player to unit
-    unit.player = i;
+    var unit = game.add.sprite(game.rnd.integerInRange(32, GAME_WIDTH - 32), 0,
+        'tank');
     // store unit under players
     players[i] = unit;
+    console.log('players[i] is', players[i]);
     // setup physics on unit
     game.physics.arcade.enable(unit);
     unit.body.gravity.y = 50;
