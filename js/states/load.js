3d8db34bb43281b568d0331173400ed9bbae86a8
AFresnedo <AFresnedo@users.noreply.github.com>
2018-08-10 13:49:24 -0700

    update filenames

--- js/states/load.js
+++ js/states/load.js
@@ -6,13 +6,13 @@ var loadState = {
 
     // load sprite for loop
     game.load.spritesheet('tank',
-        '../assets/second pass/tank-spritesheet.png', 32, 32);
+        './assets/second-pass/tank-spritesheet.png', 32, 32);
 
     // load tileset
-    game.load.image('earthTile', '../assets/earth_tileset.png');
+    game.load.image('earthTile', './assets/earth_tileset.png');
     // load tilemap
     game.load.tilemap('earth',
-        '../assets/second pass/single-level-one.json',
+        './assets/second-pass/single-level-one.json',
         null, Phaser.Tilemap.TILED_JSON);
   },
 
