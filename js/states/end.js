4441c25bbbf69a5819feb57a2fbfa80c9292f94b
AFresnedo <AFresnedo@users.noreply.github.com>
2018-08-12 21:18:21 -0700

    neaten endgame screen

--- js/states/end.js
+++ js/states/end.js
@@ -3,17 +3,18 @@ var endState = {
     // set background color to yellow
     game.stage.backgroundColor = '#ffff00';
 
-    // display victory message
-    var greetingMsg = 'congratulations!';
-    game.add.text(GAME_WIDTH / 2 - 50, GAME_HEIGHT / 2, greetingMsg);
+    // declare winner
+    var victoryMsg = 'Player ' + (winner().name + 1) + ' Has Won!';
+    game.add.text(250, 200, victoryMsg,
+        {font: 'bold 25pt Arial'});
 
     // display score
-    var scoreRecieved = 'score: ' + winner().score;
-    game.add.text(0, 0, scoreRecieved);
+    var scoreRecieved = 'Score: ' + winner().score;
+    game.add.text(325, 300, scoreRecieved);
 
-    // instructions
-    var instructions = 'press "g" to restart!';
-    game.add.text(GAME_WIDTH / 2 - 50, GAME_HEIGHT / 3, instructions);
+    // restart instructions
+    var instructions = 'Press "g" To Restart';
+    game.add.text(GAME_WIDTH / 3, GAME_HEIGHT - 50, instructions);
     var restartKey = game.input.keyboard.addKey(Phaser.Keyboard.G);
     // addOnce is a Phaser Signal, addOnce makes it a single-time trigger
     // first parameter is callback, second parameter is 'in what context'
